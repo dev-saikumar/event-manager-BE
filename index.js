523a4c2ede0512d4131b34771d54127ee31ec648
iamsaikumar <saikumarreddyatluri@gmail.com>
2019-11-10 23:17:02 +0530

    commited

--- index.js
+++ index.js
@@ -3,13 +3,12 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app= express();
 const about=require("./routes/about");
-const atended_worhshop=require("./routes/atended_worhshop");
+const atended_worhshop=require("./routes/attended_workshop");
 const getregisteredusers=require("./routes/getregistered");
 const register=require("./routes/register");
 const bodyparser= require("body-parser");
 const confirm=require("./routes/confirmworkshop");
-
-const uri="mongodb+srv://couboidsclub:audibenz@cluster0-ax1bc.mongodb.net/test?retryWrites=true&w=majority";
+const labs=require('./routes/labs');
 
 app.use(bodyparser.json());
 
@@ -18,10 +17,9 @@ app.use("/getregusers",getregisteredusers);
 app.use("/attended-workshop",atended_worhshop);
 app.use("/register",register);
 app.use("/confirm",confirm);
+app.use('/labs',labs);
 
-var url=process.env.dontKnow;
-
-mongoose.connect(url,{useNewUrlParser: true,useUnifiedTopology: true},(err,client)=>{
+mongoose.connect('mongodb+srv://couboidsclub:audibenz@cluster0-ax1bc.mongodb.net/test?retryWrites=true&w=majority',{useNewUrlParser: true,useUnifiedTopology: true},(err,client)=>{
 if(!err){
      app.listen("3000",()=>{
          app.locals.eve="flutter";
